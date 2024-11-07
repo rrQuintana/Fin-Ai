1f2a1a975e4b6d804bd3d95789438ec27346a21d
Roberto Quintana <rrquintana7@gmail.com>
2024-11-06 21:05:18 -0600

    fix types

--- src/components/CustomSelectField.tsx
+++ src/components/CustomSelectField.tsx
@@ -2,14 +2,14 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Gender } from '@/logic/types/UserOnboardingData'
 
-const CustomSelectField = ({ options, selected, onChange }: any) => {
+const CustomSelectField = ({ options, selected, onChange }: { options: string[], selected: string, onChange: (option: string) => void }) => {
   return (
     <View>
       {Object.values(options).map((option) => (
         <TouchableOpacity
           onPress={() => onChange(option)}
           className={`flex items-center0 mb-3 p-5 rounded-full ${selected === option ? 'bg-zinc-300' : 'bg-zinc-100'}`}>
-          <Text className='tex-lg font-semibold'>{option}</Text>
+          <Text className='text-lg font-semibold'>{option}</Text>
         </TouchableOpacity>
       ))}
     </View>
